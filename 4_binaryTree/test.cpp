7de1f630b7bf04ce1868424323ebdab5d8725479
space-security <a18703189601@163.com>
2022-03-20 21:24:14 +0800

    add a testing file

---
+++ 4_binaryTree/test.cpp
@@ -0,0 +1,15 @@
+/**
+ * This is a test cpp file to test binaryTree.hpp
+ */
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
